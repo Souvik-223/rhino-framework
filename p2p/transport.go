2e4cb6babf3a36e220a3f7938d09edd53b589228
Souvik Chakraborty <22souvik22@gmail.com>
2025-05-02 15:21:16 +0530

    first set

---
+++ p2p/transport.go
@@ -0,0 +1,9 @@
+package p2p
+
+// Peer is an interface that represents a remote node in the network.
+type Peer interface {
+}
+
+// Transport is anything that handles the communication between the nodes in the network.
+type Transport interface {
+}
